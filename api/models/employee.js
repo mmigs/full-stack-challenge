b6a0d5d32ce7f14fa689049196034ab81559ce57
Miguel <mmigs@users.noreply.github.com>
2018-06-16 21:53:05 -0400

    Add validation on employee table. Add password hashing

--- api/models/employee.js
+++ api/models/employee.js
@@ -1,12 +1,29 @@
 "use strict";
+const bcrypt = require("bcrypt");
+const bcrypt_p = require("bcrypt-promise");
+const jwt = require("jsonwebtoken");
+
 module.exports = (sequelize, DataTypes) => {
   var employee = sequelize.define(
     "employee",
     {
       firstName: DataTypes.STRING,
       lastName: DataTypes.STRING,
-      email: DataTypes.STRING,
-      phone: DataTypes.STRING,
+      email: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        unique: true,
+        validate: { isEmail: { msg: "Phone number invalid." } }
+      },
+      phone: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        unique: true,
+        validate: {
+          len: { args: [7, 20], msg: "Phone number invalid, too short." },
+          isNumeric: { msg: "not a valid phone number." }
+        }
+      },
       password: DataTypes.STRING,
       admin: DataTypes.BOOLEAN,
       startDate: DataTypes.DATE
@@ -24,5 +41,47 @@ module.exports = (sequelize, DataTypes) => {
       as: "reviewsWritten"
     });
   };
+
+  employee.beforeSave(async (user, options) => {
+    let err;
+    if (user.changed("password")) {
+      let salt, hash;
+      [err, salt] = await to(bcrypt.genSalt(10));
+      if (err) TE(err.message, true);
+
+      [err, hash] = await to(bcrypt.hash(user.password, salt));
+      if (err) TE(err.message, true);
+
+      user.password = hash;
+    }
+  });
+
+  employee.prototype.comparePassword = async function(pw) {
+    let err, pass;
+    if (!this.password) TE("password not set");
+
+    [err, pass] = await to(bcrypt_p.compare(pw, this.password));
+    if (err) TE(err);
+
+    if (!pass) TE("invalid password");
+
+    return this;
+  };
+
+  employee.prototype.getJWT = function() {
+    let expiration_time = parseInt(CONFIG.jwt_expiration);
+    return (
+      "Bearer " +
+      jwt.sign({ user_id: this.id }, CONFIG.jwt_encryption, {
+        expiresIn: expiration_time
+      })
+    );
+  };
+
+  employee.prototype.toWeb = function(pw) {
+    let json = this.toJSON();
+    return json;
+  };
+
   return employee;
 };
