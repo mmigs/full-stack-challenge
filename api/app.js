38c45079ce3464424c2d795d14c9d6b6372a08cb
Miguel <mmigs@users.noreply.github.com>
2018-06-17 15:40:33 -0400

    Formatting on app.js

--- api/app.js
+++ api/app.js
@@ -1,18 +1,18 @@
-require('./config/config');     //instantiate configuration variables
-require('./global_functions');  //instantiate global functions
+require("./config/config"); //instantiate configuration variables
+require("./global_functions"); //instantiate global functions
 
-console.log("Environment:", CONFIG.app)
+console.log("Environment:", CONFIG.app);
 
-const express 		= require('express');
-const logger 	    = require('morgan');
-const bodyParser 	= require('body-parser');
-const passport      = require('passport');
+const express = require("express");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
-const v1 = require('./routes/v1');
+const v1 = require("./routes/v1");
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(cookieParser());
@@ -23,41 +23,49 @@ app.use(passport.initialize());
 
 //DATABASE
 const models = require("./models");
-models.sequelize.authenticate().then(() => {
-    console.log('Connected to SQL database:', CONFIG.db_name);
-})
-.catch(err => {
-    console.error('Unable to connect to SQL database:',CONFIG.db_name, err);
-});
-if(CONFIG.app==='dev'){
-    models.sequelize.sync();//creates table if they do not already exist
-    // models.sequelize.sync({ force: true });//deletes all tables then recreates them useful for testing and development purposes
+models.sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connected to SQL database:", CONFIG.db_name);
+  })
+  .catch(err => {
+    console.error("Unable to connect to SQL database:", CONFIG.db_name, err);
+  });
+if (CONFIG.app === "dev") {
+  models.sequelize.sync(); //creates table if they do not already exist
+  // models.sequelize.sync({ force: true });//deletes all tables then recreates them useful for testing and development purposes
 }
 // CORS
-app.use(function (req, res, next) {
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization, Content-Type');
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    // Pass to next layer of middleware
-    next();
+app.use(function(req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With, content-type, Authorization, Content-Type"
+  );
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  // Pass to next layer of middleware
+  next();
 });
 
-app.use('/v1', v1);
+app.use("/v1", v1);
 
-app.use('/', function(req, res){
-	res.statusCode = 200;//send the appropriate status code
-	res.json({status:"success", message:"Parcel Pending API", data:{}})
+app.use("/", function(req, res) {
+  res.statusCode = 200; //send the appropriate status code
+  res.json({ status: "success", message: "Paytm Employee API", data: {} });
 });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
@@ -66,11 +74,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
